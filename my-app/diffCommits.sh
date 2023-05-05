64a52adcca961b42c07cf907937cc4bd04f27f33
niqitosiq <nikitashtanko@yahoo.com>
2023-05-05 19:34:16 +0300

    INITALS :DDD

--- my-app/diffCommits.sh
+++ my-app/diffCommits.sh
@@ -8,4 +8,3 @@ secondCommit=$(git rev-parse HEAD);
 node ../memory-tests/MemoryTest.js $secondCommit
 
 cd ../memory-tests && python3 createGraphs.py $firstCommit $secondCommit
-yarn diffCommits 3482e4b75a9a78b92092cfc2126ea77f306b7c81 6c744c0c92c967a16f3dc03698c7d712f4d53f69
