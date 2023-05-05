8140f5cd7c0357c2a1000d92941f4bff5c2e0163
niqitosiq <nikitashtanko@yahoo.com>
2023-05-05 14:40:20 +0300

    Reverse Anon function in prop

--- my-app/src/components/UserList.js
+++ my-app/src/components/UserList.js
@@ -3,14 +3,11 @@ import React from 'react';
 function UserList({ users, onUserSelect }) {
   return (
     <ul className="user-list">
-      {users.map((user) => {
-        const onLiClick = () => onUserSelect(user);
-        return (
-          <li key={user.id} onClick={onLiClick}>
-            {user.name}
-          </li>
-        );
-      })}
+      {users.map((user) => (
+        <li key={user.id} onClick={() => onUserSelect(user)}>
+          {user.name}
+        </li>
+      ))}
     </ul>
   );
 }
