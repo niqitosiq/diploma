9389892bb8cee714d7165ed294e020244f52ca5f
niqitosiq <nikitashtanko@yahoo.com>
2023-05-06 15:09:43 +0300

    DiffCommits Update

--- my-app/diffCommits.sh
+++ my-app/diffCommits.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
-
-git checkout $1;
-firstCommit=$(git rev-parse HEAD);
+git checkout $1
+firstCommit=$(git rev-parse HEAD)
+node ../mock-backend/index.js &
+  BACKEND_PID=$! &&
+yarn build 
+serve -s build &
+  SERVE_PID=$! &&
+echo "sleeping"
+sleep 15
+echo "starting first memory test"
 node ../memory-tests/MemoryTest.js $firstCommit
-git checkout $2;
-secondCommit=$(git rev-parse HEAD);
+echo " kill $SERVE_PID $(kill $SERVE_PID)"
+echo " kill $BACKEND_PID $(kill $BACKEND_PID)"
+
+git checkout $2
+secondCommit=$(git rev-parse HEAD)
+node ../mock-backend/index.js &
+  BACKEND_PID=$! &&
+yarn build 
+serve -s build &
+  SERVE_PID=$! &&
+echo "sleeping"
+sleep 15
+echo "starting second memory test "
 node ../memory-tests/MemoryTest.js $secondCommit
+echo " kill $SERVE_PID $(kill $SERVE_PID)"
+echo " kill $BACKEND_PID $(kill $BACKEND_PID)"
 
 cd ../memory-tests && python3 createGraphs.py $firstCommit $secondCommit
-
